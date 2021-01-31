0a19849924a24bac9913e8217a88de9525443ef7
Sunali Rambukwella <sunali.r456@gmail.com>
2021-02-01 02:02:31 +0530

    Login Page, Register Page created

--- lib/screens/landing_page.dart
+++ lib/screens/landing_page.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:selfcheckoutapp/constants.dart';
 import 'package:selfcheckoutapp/screens/home.dart';
 import 'package:selfcheckoutapp/screens/login.dart';
 
