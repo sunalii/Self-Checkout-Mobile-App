3e717b47b424309bc00abf7c05b494424dcb6f21
Sunali Rambukwella <sunali.r456@gmail.com>
2021-02-04 01:01:44 +0530

    Changes made

--- lib/constants.dart
+++ lib/constants.dart
@@ -20,4 +20,3 @@ class Constants {
     color: Colors.black
   );
 }
-
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:selfcheckoutapp/constants.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,14 +12,25 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Home',
+        style: TextStyle(color: Colors.white)
+        ),
+        backgroundColor: Color(0XFF1faa00),
+        textTheme: GoogleFonts.poppinsTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
       body: Container(
-        child: Center(
-          child: FlatButton(
-            child: Text("Logout"),
-            onPressed: (){
-              FirebaseAuth.instance.signOut();
-            },
-          )
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Text('Home'),
+                Text('Cart'),
+              ],
+            )
+          ],
         ),
       ),
     );
