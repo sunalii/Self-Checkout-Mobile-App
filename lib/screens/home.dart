99b2ca2c63854d0ad94ef2a3e44037f81b761025
Sunali Rambukwella <sunali.r456@gmail.com>
2021-02-07 13:01:55 +0530

    home page creating

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -10,26 +10,41 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  //CUSTOM HOME APP BAR
+  Widget _homeAppBar(){
+    return AppBar(
+      title: Text(
+        'Home',
+        style: Constants.boldHeadingAppBar,
+      ),
+      textTheme: GoogleFonts.poppinsTextTheme(),
+      leading: IconButton(
+        icon: Icon(Icons.logout),
+        onPressed: () {
+          FirebaseAuth.instance.signOut();
+        },
+      ),
+      flexibleSpace: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/image2.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home',
-        style: Constants.boldHeadingAppBar,
+        appBar: PreferredSize(
+          preferredSize: (Size(double.infinity, 200.0)),
+          child: _homeAppBar(),
         ),
-        textTheme: GoogleFonts.poppinsTextTheme(
-        ),
-        leading: IconButton(
-          icon: Icon(Icons.logout),
-          onPressed: (){
-            FirebaseAuth.instance.signOut();
-          },
-        ),
-      ),
-      body: ListView(
-        children: [
-        ],
-      )
-    );
+        body: ListView(
+          children: [],
+        ));
   }
 }
