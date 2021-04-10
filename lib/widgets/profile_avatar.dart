ae6950092a862b2af4f45bf1c230163f44204818
Sunali Rambukwella <sunali.r456@gmail.com>
2021-04-10 23:26:53 +0530

    profile avatar UI added

---
+++ lib/widgets/profile_avatar.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Avatar extends StatefulWidget {
+
+  @override
+  _AvatarState createState() => _AvatarState();
+}
+
+class _AvatarState extends State<Avatar> {
+
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(
+        backgroundColor: Colors.white,
+        child: Icon(
+          Icons.person,
+          size: 50,
+          color: Colors.grey,
+        ),
+      );
+  }
+}
