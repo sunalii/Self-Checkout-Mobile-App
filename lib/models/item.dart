fe8bb9fd3d9c73bb769bcc73d64609dd8485c587
Sunali Rambukwella <sunali.r456@gmail.com>
2021-04-13 01:03:44 +0530

    new user & item models added

---
+++ lib/models/item.dart
@@ -0,0 +1,7 @@
+class Item {
+  final String name;
+  final String price;
+  final int quantity;
+
+  Item({this.name, this.price, this.quantity});
+}
---
+++ lib/models/user.dart
@@ -0,0 +1,13 @@
+class Users {
+  final String uid;
+  Users({this.uid});
+}
+
+class UserData {
+  final String uid;
+  final String name;
+  final String price;
+  final int quantity;
+
+  UserData({this.uid, this.name, this.price, this.quantity});
+}
