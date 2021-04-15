302d14b274684cbb5669507aaba949adde1ea5b4
Sunali Rambukwella <sunali.r456@gmail.com>
2021-04-15 14:17:17 +0530

    widget created for Drawer

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,16 +1,13 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:selfcheckoutapp/constants.dart';
 import 'package:selfcheckoutapp/screens/bill_history.dart';
-import 'package:selfcheckoutapp/screens/edit_user_profile.dart';
 import 'package:selfcheckoutapp/screens/shopping_cart.dart';
 import 'package:selfcheckoutapp/screens/shopping_list.dart';
+import 'package:selfcheckoutapp/widgets/app_drawer.dart';
 import 'package:selfcheckoutapp/widgets/bottom_tabs.dart';
-import 'package:selfcheckoutapp/widgets/custom_button.dart';
-import 'package:selfcheckoutapp/widgets/profile_avatar.dart';
+
 
 class HomePage extends StatefulWidget {
   @override
@@ -40,104 +37,7 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
       ),
-      drawer: Drawer(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Flexible(
-              child: ListView(
-                padding: EdgeInsets.zero,
-                children: [
-                  StreamBuilder (
-                    stream: FirebaseFirestore.instance.collection('UserDetails').doc('displayName').snapshots(),
-                    builder: (context, snapshot) {
-                      if(!snapshot.hasData) {
-                        return Text("Display Name");
-                      } else {
-                        return UserAccountsDrawerHeader(
-                          decoration: BoxDecoration(
-                            color: Color(0xffD50000),
-                            image: DecorationImage(
-                                image: AssetImage("assets/image2.png"),
-                                fit: BoxFit.cover),
-                          ),
-                          accountName: StreamBuilder<Object>(
-                            stream: null,
-                            builder: (context, snapshot) {
-                              return Text(
-                                "${FirebaseAuth.instance.currentUser.displayName}",
-                                style: TextStyle(fontSize: 20.0),
-                              );
-                            }
-                          ),
-                          accountEmail: Text('${FirebaseAuth.instance.currentUser.email}'),
-                          currentAccountPicture: Avatar(),
-                        );
-                      }
-
-                    }
-                  ),
-                  ListTile(
-                    dense: true,
-                    title: Text(
-                      "Welcome to ScanGo",
-                      style:
-                          TextStyle(fontSize: 20.0, color: Color(0xff110000)),
-                    ),
-                  ),
-                  Divider(),
-                  ListTile(
-                    onTap: () {
-                      Navigator.pop(context);
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => EditUserProfile()),
-                      );
-                    },
-                    dense: true,
-                    title: Text("Account Settings",
-                        style: Constants.regularDarkText),
-                    leading: Icon(Icons.account_circle),
-                  ),
-                  ListTile(
-                    onTap: () {
-                      Navigator.pop(context);
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => HomePage()),
-                      );
-                    },
-                    dense: true,
-                    title: Text("About App", style: Constants.regularDarkText),
-                    leading: Icon(Icons.info),
-                  ),
-                  ListTile(
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                    dense: true,
-                    title: Text("Close", style: Constants.regularDarkText),
-                    leading: Icon(Icons.close_rounded),
-                  ),
-                ],
-              ),
-            ),
-            Divider(),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 20.0),
-              child: CustomBtn(
-                text: "Logout",
-                onPressed: () {
-                  FirebaseAuth.instance.signOut();
-                },
-                outlineBtn: true,
-              ),
-            ),
-          ],
-        ),
-      ),
+      drawer: AppDrawer(),
 
       body: SafeArea(
         child: Container(
