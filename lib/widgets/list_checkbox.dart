013d6d8e1169216dd66b77ab96b038e0f22725e0
Sunali Rambukwella <sunali.r456@gmail.com>
2021-04-16 16:21:47 +0530

    animated checkbox added to shopping_list

--- lib/widgets/list_checkbox.dart
+++ lib/widgets/list_checkbox.dart
@@ -7,14 +7,18 @@ class CustomCheckbox extends StatefulWidget {
   final Color selectedColor;
   final Color selectedIconColor;
 
-  CustomCheckbox({this.isChecked, this.size, this.iconSize, this.selectedColor, this.selectedIconColor});
+  CustomCheckbox(
+      {this.isChecked,
+      this.size,
+      this.iconSize,
+      this.selectedColor,
+      this.selectedIconColor});
 
   @override
   _CustomCheckboxState createState() => _CustomCheckboxState();
 }
 
 class _CustomCheckboxState extends State<CustomCheckbox> {
-
   bool _isSelected = false;
 
   @override
@@ -35,20 +39,25 @@ class _CustomCheckboxState extends State<CustomCheckbox> {
         duration: Duration(milliseconds: 500),
         curve: Curves.fastLinearToSlowEaseIn,
         decoration: BoxDecoration(
-            color: _isSelected ? widget.selectedColor ?? Color(0xff1faa00) : Colors.transparent,
+            color: _isSelected
+                ? widget.selectedColor ?? Color(0xff1faa00)
+                : Colors.transparent,
             borderRadius: BorderRadius.circular(2.0),
-            border: _isSelected ? null : Border.all(
-              color: Colors.grey,
-              width: 2.0,
-            )
-        ),
+            border: _isSelected
+                ? null
+                : Border.all(
+                    color: Colors.grey,
+                    width: 2.0,
+                  )),
         width: widget.size ?? 20,
         height: widget.size ?? 20,
-        child: _isSelected ? Icon(
-          Icons.check,
-          color: widget.selectedIconColor ?? Colors.white,
-          size: widget.iconSize ?? 18,
-        ) : null,
+        child: _isSelected
+            ? Icon(
+                Icons.check,
+                color: widget.selectedIconColor ?? Colors.white,
+                size: widget.iconSize ?? 18,
+              )
+            : null,
       ),
     );
   }
