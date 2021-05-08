1047cb03afbbdd09e58c310c10d486ce9e53e598
Sunali Rambukwella <sunali.r456@gmail.com>
2021-05-09 01:12:37 +0530

    Map data added

--- lib/models/item.dart
+++ lib/models/item.dart
@@ -7,4 +7,23 @@ class Item {
   String photo;
 
   Item({this.name, this.price, this.quantity, this.photo, this.weight});
+
+  Item.fromMap(Map map)
+      : this.name = map['name'],
+        this.barcode = map['barcode'],
+        this.price = map['price'],
+        this.quantity = map['quantity'],
+        this.photo = map['photo'],
+        this.weight = map['weight'];
+
+  Map toMap() {
+    return {
+      'name': this.name,
+      'barcode': this.barcode,
+      'price': this.price,
+      'quantity': this.quantity,
+      'photo': this.photo,
+      'weight': this.weight,
+    };
+  }
 }
