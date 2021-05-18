bc4d364f406d944dd8a7d22ee9d4ed83a9d4773d
Sunali Rambukwella <sunali.r456@gmail.com>
2021-05-18 13:38:48 +0530

    back press disabled

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -17,92 +17,95 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        title: Text(
-          'Home',
-          style: Constants.boldHeadingAppBar,
-        ),
-        textTheme: GoogleFonts.poppinsTextTheme(),
-        toolbarHeight: 200.0,
-        flexibleSpace: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20), bottomRight: Radius.circular(20)),
-              image: DecorationImage(
-                  image: AssetImage("assets/image2.png"),
-                  fit: BoxFit.cover
-              )
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          title: Text(
+            'Home',
+            style: Constants.boldHeadingAppBar,
+          ),
+          textTheme: GoogleFonts.poppinsTextTheme(),
+          toolbarHeight: 200.0,
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20), bottomRight: Radius.circular(20)),
+                image: DecorationImage(
+                    image: AssetImage("assets/image2.png"),
+                    fit: BoxFit.cover
+                )
+            ),
           ),
         ),
-      ),
-      drawer: AppDrawer(),
+        drawer: AppDrawer(),
 
-      body: SafeArea(
-        child: Container(
-          width: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                width: double.infinity,
-                padding: EdgeInsets.only(
-                  top: 30.0,
-                  left: 30.0,
+        body: SafeArea(
+          child: Container(
+            width: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.only(
+                    top: 30.0,
+                    left: 30.0,
+                  ),
+                  margin: EdgeInsets.only(bottom: 20.0),
+                  child: Text(
+                    "What do you want to do?",
+                    textAlign: TextAlign.left,
+                    style: Constants.boldHeading,
+                  ),
                 ),
-                margin: EdgeInsets.only(bottom: 20.0),
-                child: Text(
-                  "What do you want to do?",
-                  textAlign: TextAlign.left,
-                  style: Constants.boldHeading,
+                Expanded(
+                  child: Column(
+                    children: [
+                      HomeNavigateTabs(
+                        text: "Create a Shopping List",
+                        iconData: Icons.assignment_turned_in_rounded,
+                        onPressed: () {
+                          setState(() {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => ShoppingListPage()),
+                            );
+                          });
+                        },
+                      ),
+                      HomeNavigateTabs(
+                        text: "Check Cart History",
+                        iconData: Icons.history,
+                        onPressed: () {
+                          setState(() {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => BillHistoryPage()),
+                            );
+                          });
+                        },
+                      ),
+                      HomeNavigateTabs(
+                        text: "Let's Start Shopping!",
+                        iconData: Icons.shopping_cart_rounded,
+                        onPressed: () {
+                          setState(() {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => ShoppingCartPage()),
+                            );
+                          });
+                        },
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              Expanded(
-                child: Column(
-                  children: [
-                    HomeNavigateTabs(
-                      text: "Create a Shopping List",
-                      iconData: Icons.assignment_turned_in_rounded,
-                      onPressed: () {
-                        setState(() {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => ShoppingListPage()),
-                          );
-                        });
-                      },
-                    ),
-                    HomeNavigateTabs(
-                      text: "Check Cart History",
-                      iconData: Icons.history,
-                      onPressed: () {
-                        setState(() {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => BillHistoryPage()),
-                          );
-                        });
-                      },
-                    ),
-                    HomeNavigateTabs(
-                      text: "Let's Start Shopping!",
-                      iconData: Icons.shopping_cart_rounded,
-                      onPressed: () {
-                        setState(() {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => ShoppingCartPage()),
-                          );
-                        });
-                      },
-                    ),
-                  ],
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
